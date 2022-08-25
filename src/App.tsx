a8be40692af4dc32f89517f216c824ec59194b4b
jason-huski <107972974+jason-huski@users.noreply.github.com>
2022-08-25 14:59:02 +0800

    fix:update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes} from "react-router-dom";
 
 import Home from "./views/home/home";
 import About from "./views/about/about";
@@ -13,8 +13,7 @@ export default class App extends Component {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
-          <Route path="/addfriends" element={<AddFriends />}></Route>
-          <Route path="/*" element={<AddFriends />}></Route>
+          <Route path="*" element={<AddFriends />}></Route>
         </Routes>
       </BrowserRouter>
     );
