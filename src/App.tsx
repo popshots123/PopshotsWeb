bc5ae2fba43294e191622398003d0d03a9f22330
jason-huski <107972974+jason-huski@users.noreply.github.com>
2022-08-25 14:24:10 +0800

    fix:updata

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,13 @@ import AddFriends from "./views/addFriends/addFriends";
 export default class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <HashRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/*" element={<AddFriends />}></Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
     );
   }
 }
