093f7a17ba64c27024fa81be0cc90a52085e0128
jason-huski <107972974+jason-huski@users.noreply.github.com>
2022-08-25 16:24:49 +0800

    fix:updata

--- src/views/home/components/mobileHeadArea.tsx
+++ src/views/home/components/mobileHeadArea.tsx
@@ -23,7 +23,7 @@ class MobileHeadArea extends React.Component<any, any> {
     const evokeByLocation= () =>{
       console.log((window.location.href.split('?')[1]))
       const query =  window.location.href.split('?')[1] == undefined?'':'?'+window.location.href.split('?')[1]
-      let url = `https://www.popshots.xyz/#/addfriends${query}`
+      let url = `https://www.popshots.xyz/addfriends${query}`
       //let url = `/addfriends${query}`
       window.location.href = url;
       const navigate = useNavigate();
